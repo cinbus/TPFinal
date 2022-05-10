1e01f985653b1f0f4c3cc8d002d42b44c551c9c1
cinbus <95864862+cinbus@users.noreply.github.com>
2022-05-10 16:18:17 -0300

    Update java.js

--- js/java.js
+++ js/java.js
@@ -1,1 +1,25 @@
+const valorTicket=200
 
+    function calcularTotal() {
+        let cant=document.getElementById("cant").value 
+        let categoria=document.getElementById("categoria").value 
+        let descuento;
+
+        if (categoria==1) {
+            descuento=80
+        } else if (categoria==2) {
+            descuento=50
+        } else {
+            descuento=15
+        }
+            let total=cant*valorTicket
+                    
+            total-= total*descuento/100
+                
+            document.getElementById("total").innerHTML=total
+                }               
+    
+function reset1(){
+     document.getElementById("total").innerHTML="0"
+        
+  }
