e482b5127349a041ca4f143c9e69346f15063a81
cinbus <95864862+cinbus@users.noreply.github.com>
2022-08-07 12:43:17 -0300

    Add files via upload

---
+++ include/header.php
@@ -0,0 +1,113 @@
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700" rel="stylesheet">
+
+    <link rel="stylesheet" href="fonts/icomoon/style.css">
+
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    
+    <!-- Style -->
+    <link rel="stylesheet" href="css/style.css">
+
+    <title>Website Menu #4</title>
+  </head>
+  <body>
+
+
+    <div class="site-mobile-menu site-navbar-target">
+        <div class="site-mobile-menu-header">
+          <div class="site-mobile-menu-close mt-3">
+            <span class="icon-close2 js-menu-toggle"></span>
+          </div>
+        </div>
+        <div class="site-mobile-menu-body"></div>
+      </div>
+
+
+      <div class="top-bar">
+        <div class="container">
+          <div class="row">
+            <div class="col-12">
+              <a href="#" class=""><span class="mr-2  icon-envelope-open-o"></span> <span class="d-none d-md-inline-block">info@yourdomain.com</span></a>
+              <span class="mx-md-2 d-inline-block"></span>
+              <a href="#" class=""><span class="mr-2  icon-phone"></span> <span class="d-none d-md-inline-block">1+ (234) 5678 9101</span></a>
+
+
+              <div class="float-right">
+
+                <a href="#" class=""><span class="mr-2  icon-twitter"></span> <span class="d-none d-md-inline-block">Twitter</span></a>
+                <span class="mx-md-2 d-inline-block"></span>
+                <a href="#" class=""><span class="mr-2  icon-facebook"></span> <span class="d-none d-md-inline-block">Facebook</span></a>
+
+              </div>
+
+            </div>
+
+          </div>
+
+        </div>
+      </div>
+
+      <header class="site-navbar js-sticky-header site-navbar-target" role="banner">
+
+        <div class="container">
+          <div class="row align-items-center position-relative">
+
+
+            <div class="site-logo">
+            <img class="d-block mx-auto mb-2" src="./images/logo.jpg" alt="Logo caba" width="72" height="72">
+            </div>
+
+            <div class="col-12">
+              <nav class="site-navigation text-right ml-auto " role="navigation">
+
+                <ul class="site-menu main-menu js-clone-nav ml-auto d-none d-lg-block">
+                  <li><a href="inicio.php" class="nav-link">Home</a></li>
+                  
+                  <li class="has-children">
+                    <a href="#" class="nav-link">Usuarios</a>
+                    <ul class="dropdown arrow-top">
+                      <li><a href="altaUsuario.php" class="nav-link">Crear Usuarios</a></li>
+                      <li><a href="listadoUsuarios.php" class="nav-link">Listar Usuarios</a></li>
+                      
+                    </ul>
+                  </li>
+
+                  <li class="has-children">
+                    <a href="#" class="nav-link">Pedidos</a>
+                    <ul class="dropdown arrow-top">
+                      <li><a href="listadoPedidos.php" class="nav-link">Listado de Pedidos</a></li>
+                      <li><a href="pedido.php" class="nav-link">Cargar Pedido</a></li>
+
+                    </ul>
+                  </li>
+
+                  <li class="has-children">
+                    <a href="#" class="nav-link">Acerca de</a>
+                    <ul class="dropdown arrow-top">
+                      <li><a href="#" class="nav-link">Nosotros</a></li>
+                      <li><a href="#" class="nav-link">Nuestra Misión</a></li>
+                      <li><a href="#" class="nav-link">Historia</a></li>
+                      
+                    </ul>
+                  </li>
+
+                  <li><a href="#why-us-section" class="nav-link">Contacto</a></li>
+                  
+                </ul>
+              </nav>
+
+            </div>
+
+            <div class="toggle-button d-inline-block d-lg-none"><a href="#" class="site-menu-toggle py-5 js-menu-toggle text-black"><span class="icon-menu h3"></span></a></div>
+
+          </div>
+        </div>
+
+      </header>
